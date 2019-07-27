fac13fd5a9daba8e00d9209a2af03ce198a43462
mooss <mooss@protonmail.com>
2019-07-27 17:25:04 +0200

    Add pdf generation

---
+++ Makefile
@@ -0,0 +1,19 @@
+pdf: cpp_core_guidelines.pdf
+
+cpp_core_guidelines.md: CppCoreGuidelines.md
+	perl instantiate_code_blocks.pl $^ > $@
+
+cpp_core_guidelines.pdf: cpp_core_guidelines.md
+	pandoc 	--standalone\
+		--to latex $^\
+		--output $@\
+		--include-in-header preamble.tex\
+		--highlight-style tango\
+		--variable documentclass:report\
+		--variable geometry:margin=2.5cm\
+		--variable papersize:a4paper\
+		--table-of-content\
+		--variable lang:en
+
+clean:
+	rm cpp_core_guidelines.{pdf,md} -f
---
+++ instantiate_code_blocks.pl
@@ -0,0 +1,62 @@
+use strict;
+use warnings;
+use v5.14;
+
+# Transform the tab-based code blocks of a markdown file into specific C++ code blocks.
+# Used on CppCoreGuidelines in conjunction with pandoc in order to produce a prettified pdf suiting my taste.
+
+my @codelines = ();
+my $current_indentation = 0;
+while(my $line = <>){
+    my $indentation = 0;
+    if($line =~ m/^( +)/){
+        $indentation = length($1);
+    }
+
+    my $inside_code_block = $indentation >= $current_indentation || $line =~ m/^$/;
+
+    if($inside_code_block){
+        if(@codelines > 0){
+            # Code block already started.
+            $line =~ s/^    //;
+            push @codelines, $line;
+        }
+        elsif($indentation < 4){
+            # Not inside pre-existing code block and empty line.
+            print $line;
+        }
+        else{
+            # First proper line of code block.
+            $current_indentation = $indentation;
+            $line =~ s/^    //;
+            push @codelines, $line;
+        }
+    }
+    else{
+        if(@codelines > 0){
+            # Do not include leading and trailing empty lines into the code block.
+            my @head = ();
+            my @tail = ();
+            while(1){
+                last if @codelines == 0 or $codelines[0] ne "\n";
+                push @head, shift @codelines;
+            }
+            while(1){
+                last if @codelines == 0 or $codelines[-1] ne "\n";
+                push @tail, pop @codelines;
+            }
+
+            # Print head, codeblock proper and tail.
+            print join "", @head;
+            if(@codelines > 0){
+                say "```C++";
+                print join "", @codelines;
+                say "```";
+            }
+            print join "", @tail;
+            @codelines = ();
+            $current_indentation = 0;
+        }
+        print $line;
+    }
+}
